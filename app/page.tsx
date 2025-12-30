14ebd04ba361f8b9505f67fda94d401eab0d9346
xXJoaoVictorNogXx <joaovictorfcantanheden@gmail.com>
2025-12-30 20:03:23 -0300

    Inicio de alterações

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       name: "Tábua Romântica",
       description: "Queijo brie, presunto parma, morangos e mel.",
       price: 120.0,
-      image: "/images/tabua1.jpg", // Você coloca a foto na pasta public/images
+      image: "/images/Tabua P.jpg",
       category: "Casal",
     },
     {
@@ -19,12 +19,42 @@ export default function Home() {
       image: "/images/tabua2.jpg",
       category: "Família",
     },
-    // ... mais produtos
+    {
+      id: 3,
+      name: "Tábua Família",
+      description: "Mix de salames, 4 tipos de queijos e pães artesanais.",
+      price: 250.0,
+      image: "/images/tabua2.jpg",
+      category: "Família",
+    },
+    {
+      id: 4,
+      name: "Tábua Família",
+      description: "Mix de salames, 4 tipos de queijos e pães artesanais.",
+      price: 250.0,
+      image: "/images/tabua2.jpg",
+      category: "Família",
+    },
+    {
+      id: 5,
+      name: "Tábua Família",
+      description: "Mix de salames, 4 tipos de queijos e pães artesanais.",
+      price: 250.0,
+      image: "/images/tabua2.jpg",
+      category: "Família",
+    },
+    {
+      id: 6,
+      name: "Tábua Família",
+      description: "Mix de salames, 4 tipos de queijos e pães artesanais.",
+      price: 250.0,
+      image: "/images/tabua2.jpg",
+      category: "Família",
+    },
   ];
 
   return (
     <main className="min-h-screen  pb-10 bg-linear-to-b from-olive via-green-900 to-green-950">
-      {/* Cabeçalho simples */}
       <header className="p-4 text-white text-center flex flex-col items-center">
         <Image
           src="/logo.png"
@@ -34,21 +64,16 @@ export default function Home() {
         />
       </header>
 
-      {/* Lista de Produtos */}
       <div className="p-4">
-        <h2 className="text-lg font-semibold text-beige mb-4">
-          Nosso Cardápio
-        </h2>
+        <h2 className="text-lg font-serif text-beige mb-4">Nosso Cardápio</h2>
 
-        {/* GRID: 1 coluna no celular (padrão), 2 colunas se tela for maior (sm) */}
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
           {products.map((product) => (
             <div
               key={product.id}
               className="bg-white rounded-xl shadow-sm border border-gray-100 overflow-hidden"
             >
-              {/* Foto do Produto */}
-              <div className="relative h-30 w-full">
+              <div className="relative h-48 w-full">
                 <Image
                   src={product.image}
                   alt={product.name}
@@ -57,23 +82,21 @@ export default function Home() {
                 />
               </div>
 
-              {/* Informações */}
               <div className="p-4">
                 <div className="flex justify-between items-start">
-                  <h3 className="font-bold text-gray-900">{product.name}</h3>
+                  <h3 className="font-serif text-gray-900">{product.name}</h3>
                   <span className="bg-green-100 text-green-800 text-xs px-2 py-1 rounded-full font-bold">
                     R$ {product.price}
                   </span>
                 </div>
-                <p className="text-gray-500 text-sm mt-1">
+                <p className="text-gray-500 font-serif text-sm mt-1">
                   {product.description}
                 </p>
 
-                {/* Botão de WhatsApp (Essencial para fechar venda!) */}
                 <a
                   href={`https://wa.me/5598999999999?text=Olá, quero pedir a ${product.name}`}
                   target="_blank"
-                  className="mt-4 block w-full bg-green-600 text-white text-center py-3 rounded-lg font-semibold hover:bg-green-700 transition"
+                  className="mt-4 block w-full bg-green-600 text-white text-center py-3 rounded-lg font-serif hover:bg-green-700 transition"
                 >
                   Pedir no WhatsApp
                 </a>
